e134643316ae383340ed3d3239d3eff11a6b1d36
Jafar Mohamed <jafar.mohamed2006@gmail.com>
2026-05-01 02:29:48 +0100

    Completed calculator project

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,6 +1,8 @@
+import art
+print(art.logo)
+
 def add(n1, n2):
     return n1 + n2
-calc_no_1 = add
 
 
 def subtract(n1,n2):
@@ -13,11 +15,31 @@ def divide(n1,n2):
     return n1 / n2
 
 Math_operations = {
-    "addition" : [add(4,8)],
-    "subtraction" : [subtract(4,8)],
-    "multiplication" : [multiply(4,8)],
-    "division" : [divide(4,8)]
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
 }
-print(Math_operations)
+def calculator():
+    print(art.logo)
+    no_1 = float(input("What is the first number?"))
+    continue_calculation = True
+    while continue_calculation:
+        for symbol in Math_operations:
+            print(symbol)
 
+        chosen_operation = input("Pick an operation: ")
+        no_2 = float(input("What is the second number?"))
+        result = Math_operations[chosen_operation](no_1, no_2)
+        print(f"{no_1} {chosen_operation} {no_2} = {result}")
 
+        previous_answer = result
+        new_input = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation:").lower()
+        if new_input == "y":
+            no_1 = previous_answer
+
+        else:
+            continue_calculation = False
+            print("\n" *20)
+            calculator()
+calculator()
