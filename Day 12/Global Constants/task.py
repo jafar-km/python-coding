4d992784f8cc4704626befa41731ac39ec70a77b
Jafar Mohamed <jafar.mohamed2006@gmail.com>
2026-05-04 23:54:28 +0100

    Day 12 Blackjack project completed

---
+++ Day 12/Global Constants/task.py
@@ -0,0 +1,6 @@
+# Global Constants
+
+PI = 3.14159
+
+def my_funct():
+    print(PI)
--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -1,10 +1,28 @@
-enemies = 1
+# enemies = 1
+#
+#
+# def increase_enemies():
+#     enemies = 2
+#     print(f"enemies inside function: {enemies}")
+#
+#
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
 
+# Local Scope
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+#
+# drink_potion()
+# print(potion_strength)
 
-def increase_enemies():
-    enemies = 2
-    print(f"enemies inside function: {enemies}")
+#Global Scope
+player_health = 10
 
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+drink_potion()
+print(player_health)
