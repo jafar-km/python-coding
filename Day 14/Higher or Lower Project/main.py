254eb9edb173945eec7b39cdfa6c0b4a67cd7e06
Jafar Mohamed <jafar.mohamed2006@gmail.com>
2026-05-12 23:59:23 +0100

    Starting the project higher or lower

---
+++ Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,18 @@
+import random
+
+import art
+import game_data
+
+Celebrity = []
+
+
+# game_is_on = True
+# while game_is_on:
+print(art.logo)
+# for person in game_data.data:
+random_person = random.choice(game_data.data)
+Celebrity.append(random_person)
+name = Celebrity["name"]
+
+
+print(Celebrity[0][""])
