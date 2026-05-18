8e5b6bf6143d6a10538a8b2764aff079e12bf236
Jafar Mohamed <jafar.mohamed2006@gmail.com>
2026-05-18 23:59:23 +0100

    project day 15

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -25,8 +25,49 @@ MENU = {
 }
 
 resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+    "Water": 300,
+    "Milk": 200,
+    "Coffee": 100,
 }
+
+
+money = 0
+
+COIN_VALUES = {
+    "quarters": 0.25,
+    "dimes": 0.10,
+    "nickles": 0.05,
+    "pennies": 0.01,
+}
+
+customer_input = input("What would you like? (espresso/latte/cappuccino): ")
+
+if customer_input == "off":
+    exit()
+
+elif customer_input == "report":
+    for item, amount in resources.items():
+        print(f"{item.title()}: {amount}")
+
+    print(f"Money: ${money}")
+
+elif customer_input in ["espresso", "latte", "cappuccino"]:
+
+    print("Please insert coins.")
+
+    quarters = int(input("How many quarters? "))
+    dimes = int(input("How many dimes? "))
+    nickles = int(input("How many nickles? "))
+    pennies = int(input("How many pennies? "))
+
+    total = (
+        quarters * COIN_VALUES["quarters"]
+        + dimes * COIN_VALUES["dimes"]
+        + nickles * COIN_VALUES["nickles"]
+        + pennies * COIN_VALUES["pennies"]
+    )
+
+    money += total
+
+    MENU[customer_input]["ingredients"]
 
