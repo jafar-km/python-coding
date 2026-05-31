57e03a608b10380d7228eb6888783d35a5ac1498
Jafar Mohamed <jafar@MacBook-Neo.local>
2026-06-01 00:00:20 +0100

    day 16 start

---
+++ Day 16 /oop-coffee-machine-start/main.py
@@ -0,0 +1,14 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on = True
+
+
+
+coffee_maker.report()
+money_machine.report()
+
+while is_ongit add.
