3b7a40f60d2301c196f58a5a68c556b7443992bc
Jafar Mohamed <jafar.mohamed2006@gmail.com>
2026-06-13 22:51:33 +0100

    Day 22 Pong Project

---
+++ Day 22/main.py
@@ -0,0 +1,54 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+
+l_paddle = Paddle((350, 0))
+r_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    #Detect Collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        #needs to bounce
+        ball.bounce_y()
+
+    #Detect collision with paddle (reason why 50 and 340 were used was to solve the issue of how python measures the distance between two turtles from its center, how far to the right/ left and of distance with the paddle )
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+
+    #Detect when R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+
+    #Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
